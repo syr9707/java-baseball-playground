004534607eab26ea2ffa925912366bcc70778f32
syr9707 <yrshin9771@gmail.com>
2023-03-10 13:27:29 +0900

    feat (Random Number): 컴퓨터의 랜덤 숫자 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Application {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> computerNum = new ArrayList<>();
+        while(computerNum.size() < 3) {
+            int num = (int)(Math.random() * 9) + 1;
+            if(!computerNum.contains(num)) computerNum.add(num);
+        }
+
+        System.out.print("숫자를 입력해 주세요 : ");
+        String numStr = br.readLine();
+
+
+    }
+
 }
