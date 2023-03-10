fa1e492788eb92dc7753c125efbed86920e3390e
syr9707 <yrshin9771@gmail.com>
2023-03-10 13:48:24 +0900

    feat (play judge): 플레이어의 숫자 결과 판단

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,8 +15,20 @@ public class Application {
 
         System.out.print("숫자를 입력해 주세요 : ");
         String numStr = br.readLine();
+        judgeNumber(numStr, computerNum);
 
+    }
 
+    public static void judgeNumber(String numStr, ArrayList<Integer> computerNum) {
+        int countStrike = 0;
+        int countBall = 0;
+
+        for(int i = 0; i < 3; i++) {
+            if(computerNum.contains(numStr.charAt(i) - '0')) countBall++;
+            if(computerNum.get(i) == numStr.charAt(i) - '0') countStrike++;
+        }
+        countBall = countBall - countStrike;
+        printResult(countStrike, countBall);
     }
 
 }
